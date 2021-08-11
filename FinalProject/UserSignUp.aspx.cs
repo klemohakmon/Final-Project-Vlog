1cf77ed7e56d14e5dc01f5fe184f0187f8c41792
Klemo Hakmon <73479885+klemohakmon@users.noreply.github.com>
2021-08-11 20:00:34 +0300

    Data Base Connection User Sign Up 2

--- FinalProject/UserSignUp.aspx.cs
+++ FinalProject/UserSignUp.aspx.cs
@@ -21,6 +21,60 @@ namespace FinalProject
         // sign up button click event
         protected void Button1_Click(object sender, EventArgs e)
         {
+            if (checkMemberExsists())
+            {
+                Response.Write("<script>alert('Member Already  Exist with this Member ID, Pls try other ID ');</script>");
+
+            }
+            else
+            {
+                signUpNewMember();
+            }
+            
+        }
+
+
+
+        //user defined method
+        bool checkMemberExsists() 
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("SELECT * from Users where User_id = '" + TextBox8.Text.Trim() +"';", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+
+                if (dt.Rows.Count >= 1)     
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+    
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+                return false;
+            }
+            
+        }
+
+
+        void signUpNewMember()
+        {
+
             try
             {
                 SqlConnection con = new SqlConnection(strcon);
@@ -35,15 +89,15 @@ namespace FinalProject
                 "(@Full_Name,@Phone_Number,@State,@City,@Zip_Code,@Birth_Date," +
                 "@Full_Adrress,@E_Mail,@Password,@Member_id) ", con);
 
-                cmd.Parameters.AddWithValue("@Full_Name",TextBox1.Text.Trim());
-                cmd.Parameters.AddWithValue("@Phone_Number",TextBox3.Text.Trim());
+                cmd.Parameters.AddWithValue("@Full_Name", TextBox1.Text.Trim());
+                cmd.Parameters.AddWithValue("@Phone_Number", TextBox3.Text.Trim());
                 cmd.Parameters.AddWithValue("@State", DropDownList1.SelectedItem.Value);
-                cmd.Parameters.AddWithValue("@City",TextBox6.Text.Trim());
-                cmd.Parameters.AddWithValue("@Zip_Code",TextBox7.Text.Trim());
+                cmd.Parameters.AddWithValue("@City", TextBox6.Text.Trim());
+                cmd.Parameters.AddWithValue("@Zip_Code", TextBox7.Text.Trim());
                 cmd.Parameters.AddWithValue("@Birth_Date", TextBox2.Text.Trim());
-                cmd.Parameters.AddWithValue("@Full_Adrress",TextBox5.Text.Trim());
-                cmd.Parameters.AddWithValue("@E_Mail",TextBox4.Text.Trim());
-                cmd.Parameters.AddWithValue("@Password",TextBox9.Text.Trim());
+                cmd.Parameters.AddWithValue("@Full_Adrress", TextBox5.Text.Trim());
+                cmd.Parameters.AddWithValue("@E_Mail", TextBox4.Text.Trim());
+                cmd.Parameters.AddWithValue("@Password", TextBox9.Text.Trim());
                 cmd.Parameters.AddWithValue("@Member_id", TextBox8.Text.Trim());
 
                 cmd.ExecuteNonQuery();
@@ -56,7 +110,7 @@ namespace FinalProject
             catch (Exception ex)
             {
                 Response.Write("<script>alert('" + ex.Message + "');</script>");
-               
+
             }
         }
     }
