bc23f0fc594806d6e9cb5671feb39460adb15b3a
Klemo Hakmon <73479885+klemohakmon@users.noreply.github.com>
2021-08-30 10:53:42 +0300

    sqlqueryfinalchange:)

--- FinalProject/UserSignUp.aspx.cs
+++ FinalProject/UserSignUp.aspx.cs
@@ -46,7 +46,7 @@ namespace FinalProject
                     con.Open();
                 }
 
-                SqlCommand cmd = new SqlCommand("SELECT * from members_id where member_id = '" + TextBox8.Text.Trim() +"';", con);
+                SqlCommand cmd = new SqlCommand("SELECT * from members_tbl where member_id = '" + TextBox8.Text.Trim() +"';", con);
                 SqlDataAdapter da = new SqlDataAdapter(cmd);
                 DataTable dt = new DataTable();
                 da.Fill(dt);
